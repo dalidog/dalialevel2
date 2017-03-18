e1d2b454c9051a289a41c7a66abbaa6ccf21588e
dalidog <league@WTS-iMac-11.attlocal.net>
2017-03-17 18:30:46 -0700

    dfrghjk,jmngbvfcdxs

--- src/dalialevel2/LeagueInvaders.java
+++ src/dalialevel2/LeagueInvaders.java
@@ -2,11 +2,14 @@ package dalialevel2;
 
 import javax.swing.Timer;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.text.Normalizer.Form;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -40,26 +43,67 @@ public class LeagueInvaders {
 
 class GamePanel extends JPanel implements ActionListener, KeyListener {
 	Timer timer = new Timer((1000 / 60), this);
-	GameObject GameObject = new GameObject();
-
+	final int MENU_STATE = 0;
+	final int GAME_STATE = 1;
+	final int END_STATE = 2;
+	Font font = new Font("Papyrus", Font.PLAIN, 48);
+	int currentState = MENU_STATE;
 	public GamePanel() {
 		this.timer = timer;
-		this.GameObject = GameObject;
+		
 	}
-
+	public void updateMenuState(){
+		
+	}
+	public void updateGameState(){
+		
+	}
+	public void updateEndState(){
+		
+	}
+	public void drawMenuState(Graphics g){
+		g.setColor(Color.MAGENTA);
+		g.fillRect(0, 0, 500, 800);
+		g.setFont(font); 
+		g.setColor(Color.black);
+		g.drawString("League Invaders!", 100, 100);
+	}
+	public void drawGameState(Graphics g){
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, 500, 800);
+	}
+	public void drawEndState(Graphics g){
+		g.setColor(Color.BLUE);
+		g.fillRect(0, 0, 500, 800);
+	}
 	public void startGame() {
 		timer.start();
 	}
 
 	public void paintComponent(Graphics g) {
-		GameObject.draw(g);
+		if(currentState == MENU_STATE){
+			drawMenuState(g);
+		}else if(currentState == GAME_STATE){
+			drawGameState(g);
+		}else if(currentState == END_STATE){
+			drawEndState(g);
+		}
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		repaint();
-		GameObject.update();
+
+		if(currentState == MENU_STATE){
+			updateMenuState();
+		}else if(currentState == GAME_STATE){
+			updateGameState();
+		}else if(currentState == END_STATE){
+			updateEndState();
+		}
+
+		
 	}
 
 	@Override
@@ -72,6 +116,14 @@ class GamePanel extends JPanel implements ActionListener, KeyListener {
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
 		System.out.println("b");
+		if(e.getKeyCode()==10){
+			currentState += 1;
+			 if(currentState >  END_STATE){
+				currentState = MENU_STATE;
+			}
+
+
+		}
 	}
 
 	@Override
